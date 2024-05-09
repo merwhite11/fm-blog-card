bb61a606e4867cbd8265f55030700ae2e3644672
merwhite11 <meredith.white91@gmail.com>
2024-05-09 10:24:13 -0700

    Working on responsive image and card size

--- blog-card-app/src/App.js
+++ blog-card-app/src/App.js
@@ -1,24 +1,38 @@
-import './App.css';
-import CardImage from "./assets/images/illustration-article.svg"
-
+import "./App.css";
+import CardImage from "./assets/images/illustration-article.svg";
+import FooterImage from "./assets/images/image-avatar.webp";
 function App() {
   return (
     <div className="App">
       <section className="card">
-        <div className="card___body">
-          <img className="card__image" src={CardImage} alt="abstract">
+        <div className="card__body">
+          <div className="card__image-container">
 
-          </img>
-          <div className="card__content">
-            <div className="card__flag"></div>
-            <p className="card__dateline"></p>
-            <h1>HTML & CSS foundations</h1>
-            <p className="card__text">These languages are the backbone of every website, definiting structure, content, and presentation.</p>
-            <div className="card__footer"></div>
+          <img className="card__image" src={CardImage} alt="abstract"></img>
           </div>
-
+          <div className="card__content">
+            <div className="card__flag">Learning</div>
+            <p className="card__dateline">Published 21 Dec 2023</p>
+            <h1>HTML & CSS foundations</h1>
+            {/* <p className="card__text">
+              These languages are the backbone of every website, definiting
+              structure, content, and presentation.
+            </p> */}
+            <div className="card__footer">
+              <img
+                className="card__footer-image"
+                src={FooterImage}
+                alt="avatar"
+              ></img>
+              <p className="card__footer-byline">Greg Hooper</p>
+            </div>
+          </div>
         </div>
       </section>
+      {/* <div class="attribution">
+    Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>.
+    Coded by <a href="#" v>Meredith White</a>.
+    </div> */}
     </div>
   );
 }
